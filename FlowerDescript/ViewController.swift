3474eaa34a12a933a8d212e3250eb1bdf21f2488
milhamat <M477ashiddiq@gmail.com>
2021-02-13 17:15:14 +0700

    first final upload

--- FlowerDescript/ViewController.swift
+++ FlowerDescript/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        requestInfo(flowerName: "Barberton Daisy")
     }
 
+    func requestInfo(flowerName: String){
+        
+        let parameters : [String:String] = [
+          "format" : "json",
+          "action" : "query",
+          "prop" : "extracts|pageimages",
+          "exintro" : "",
+          "explaintext" : "",
+          "titles" : flowerName,
+          "indexpageids" : "",
+          "redirects" : "1",
+            "pithumbsize" : "500"
+          ]
+        
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON {
+            (response) in
+            if response.result.isSuccess {
+                print("got the wikipedia info.")
+                print(response)
+                
+                let flowerJSON: JSON  = JSON(response.result.value!)
+                
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                
+                self.textView.text = flowerDescription
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                
+                print("Description: \(flowerDescription)")
+            }
+        }
+    }
 
 }
 
---
+++ Pods/Target Support Files/Pods-FlowerDescript/Pods-FlowerDescript-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlowerDescript : NSObject
+@end
+@implementation PodsDummy_Pods_FlowerDescript
+@end
